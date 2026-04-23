6d171be7094541201e94bcec31384e18a286b229
Samuel Lima <samuel.aguiar0106@gmail.com>
2026-04-23 17:45:17 -0300

    Final script POO

--- Arquivo_POO.py
+++ Arquivo_POO.py
@@ -1,14 +1,23 @@
 #CONTA DE BANCO
 
 nome = input("Digite seu nome: ")
-saldo = input("Digite seu saldo atual: ")
+saldo = int(input("Digite seu saldo atual: "))
+am = int(input("Digite o valor que queres depositar: "))
+
+
 
 class Banco:
     def __init__(self, n, s):
         self.nome = n
         self.saldo = s
+    def depositar(self, saldo1):
+        self.saldo += saldo1
+    def exibir(self):
+        print(f"o nome do usuuário é {self.nome}")
+        print(f"O saldo é igual a: {self.saldo}")
+
 
 
 pessoa1 = Banco(nome, saldo)
-print( pessoa1.nome)
-print(pessoa1.saldo)
+pessoa1.depositar(am)
+pessoa1.exibir()
