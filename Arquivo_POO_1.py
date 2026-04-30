e30d75b75230eeeaf38c1afb98050ccd4405f086
Samuel Lima <samuel.aguiar0106@gmail.com>
2026-04-29 21:53:40 -0300

    Script_POO_1.2

--- Arquivo_POO_1.py
+++ Arquivo_POO_1.py
@@ -5,22 +5,32 @@ valor = int(input("Digite o saldo inicial do usuário: "))
 taxa = int(input("Digite a taxa de juros: "))
 meses = int(input("Digite o numero de meses: "))
 
-val_bruto= 0
-val_liquido = 0
 
 class Sistema:
-    def _init_(self, n, v, t, m, b, l):
+    def __init__(self, n, v, t, m):
         self.valor = v
         self.nome = n
         self.meses = m
         self.taxa = t
-        self.bruto = b
-        self.liquido = l
+        self.bruto = 0
+        self.liquido = 0
 
-    def juros_compostos(self):
-        bruto = self.valor*((1+self.taxa)**self.meses)
+    def valor_bruto(self):
+        bruto = self.valor*((1+self.taxa/100)**self.meses)
         self.bruto = bruto
 
+    def valor_liquido(self):
+        lucro = self.bruto -  self.valor
+        imposto = lucro * 0.15
+        self.liquido = self.bruto - imposto
 
-pessoa1 = Sistema(nome, valor, taxa, meses, val_bruto, val_liquido)
+    def exibir(self):
+        print(f"Usuário: {self.nome}")
+        print(f"Montante bruto do valor inicial é de: {self.bruto}")
+        print(f"Valor Liquido final: {self.liquido}")
+
+pessoa1 = Sistema(nome, valor, taxa, meses)
+pessoa1.valor_bruto()
+pessoa1.valor_liquido()
+pessoa1.exibir()
 
