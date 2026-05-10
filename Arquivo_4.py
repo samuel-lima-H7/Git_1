1030b50bfd5c2960a26abc5dee9f1c2717b48da7
Samuel Lima <samuel.aguiar0106@gmail.com>
2026-05-10 18:17:21 -0300

    Arquivo_4_resolver depois

--- Arquivo_4.py
+++ Arquivo_4.py
@@ -4,28 +4,31 @@ quant = int(input("Digite a quantidade de veiculos que deseja cadastrar: "))
 veiculos = list()
 
 
-def armazenamento():
-    for c in range(quant):
-        dados_veiculo = list()
-        print("-="*20)
-        veiculo = input("Digite o veiculo de registro: ")
-        km_rodados = float(input("Digite o numero de quilometros que o veiculo percorreu: "))
-        comb_usado = float(input("Digite a quantidade em litros de combustível usado: "))
-        temp_gasto = float(input("Digite o tempo em horas de viagem que o veiculo percorreu: "))
-        print("-=" * 20)
-        dados_veiculo.append(veiculo)
-        dados_veiculo.append(km_rodados)
-        dados_veiculo.append(comb_usado)
-        dados_veiculo.append(temp_gasto)
-        veiculos.append(dados_veiculo)
+
+for c in range(quant):
+    dados_veiculo = list()
+    print("-="*20)
+    veiculo = input("Digite o veiculo de registro: ")
+    km_rodados = float(input("Digite o numero de quilometros que o veiculo percorreu: "))
+    comb_usado = float(input("Digite a quantidade em litros de combustível usado: "))
+    temp_gasto = float(input("Digite o tempo em horas de viagem que o veiculo percorreu: "))
+    print("-=" * 20)
+    dados_veiculo.append(veiculo)
+    dados_veiculo.append(km_rodados)
+    dados_veiculo.append(comb_usado)
+    dados_veiculo.append(temp_gasto)
+    veiculos.append(dados_veiculo)
 
 def analise_repeticao():
     repetido = 0
     for r in range(len(veiculos)):
         nome_atual = veiculos[r][0]
+        veiculos.pop(r)
         for v in range(len(veiculos)):
             if nome_atual == veiculos[v][0]:
                 repetido += 1
+                print(nome_atual)
+    print(repetido)
 
 
 def analise_manutencao(val,b):
@@ -41,13 +44,15 @@ def analise_eficiencia(val, b):
     else:
         return "Veiculo NÃO Eficiente"
 
-armazenamento()
-
-for a in range(len(veiculos)):
+'''for a in range(len(veiculos)):
     print("-="*20)
     print(f"O Veiculo {veiculos[a][0]:^20}")
     print(analise_eficiencia(veiculos, a))
     print(analise_manutencao(veiculos, a))
+'''
+
+analise_repeticao()
+
 
 
 
