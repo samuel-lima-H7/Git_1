dc2f2ff9327b88dd84ad4517a2a3f1dff8a02993
Samuel Lima <samuel.aguiar0106@gmail.com>
2026-05-15 10:00:19 -0300

    Arquivo_4_commit

--- Arquivo_4.py
+++ Arquivo_4.py
@@ -19,17 +19,7 @@ for c in range(quant):
     dados_veiculo.append(temp_gasto)
     veiculos.append(dados_veiculo)
 
-def analise_repeticao():
-    repetido = 0
-    for r in range(len(veiculos)):
-        nome_atual = veiculos[r][0]
-        veiculos.pop(r)
-        for v in range(len(veiculos)):
-            if nome_atual == veiculos[v][0]:
-                repetido += 1
-                print(nome_atual)
-    print(repetido)
-
+total = len(veiculos)
 
 def analise_manutencao(val,b):
     if val[b][1] >= 5000.00:
@@ -51,7 +41,20 @@ def analise_eficiencia(val, b):
     print(analise_manutencao(veiculos, a))
 '''
 
-analise_repeticao()
+
+repetido = 0
+for r in range(len(veiculos)):
+    lista_de_veiculos = list()
+    veiculos_copia = veiculos[:]
+    nome_atual = veiculos[r][0]
+    for v in range(len(lista_de_veiculos)):
+        print(lista_de_veiculos[v][0])
+        print(nome_atual)
+        if nome_atual == lista_de_veiculos[v][0]:
+            repetido += 1
+            print(repetido)
+
+
 
 
 
