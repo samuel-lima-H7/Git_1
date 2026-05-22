224946f06db4bf62c1e223b30490e39633f0e2a6
Samuel Lima <samuel.aguiar0106@gmail.com>
2026-05-22 10:37:10 -0300

    Arquivo_notifica commit 3

--- Arquivo_Notifica.py
+++ Arquivo_Notifica.py
@@ -1,27 +1,32 @@
 #ENVIA CRONOGRAMA PARA O EMAIL
 
+from _datetime import datetime
 import csv
-import os
+
 
 quant =  int(input("Digite o numero de tarefa que deseja fazer hoje: "))
+dia =  input("Digite o dia da semana do qual a tarefa fará parte: ")
 lista_tarefa =  list()
 
-for c in range(quant):
-    dados  = list()
-    print("-="*20)
-    hora = input("Digite o inicio do compromisso: ")
-    compromisso = input("Digite o compromisso: ")
-    print("-=" * 20)
-    dados.append(hora)
-    dados.append(compromisso)
+def info():
+    lista_tarefa.append(dia)
+    for c in range(quant):
+        dados  = list()
+        print("-="*20)
+        hora = input("Digite o inicio do compromisso: ")
+        compromisso = input("Digite o compromisso: ")
+        print("-=" * 20)
+        dados.append(hora)
+        dados.append(compromisso)
+        lista_tarefa.append(dados)
 
-    lista_tarefa.append(dados)
+def armazena():
+    with open("C:/Users/user/Desktop/Programação/teste programação.csv","w", newline="", encoding="utf-8-sig") as arquivo:
+        planilha = csv.writer(arquivo, delimiter=";")
+        planilha.writerow(["Dia da Semana","Hora", "Tarefa"])
 
-with open("C:/Users/user/Desktop/Programação/teste programação.csv","w", newline="", encoding="utf-8-sig") as arquivo:
-    planilha = csv.writer(arquivo, delimiter=";")
-    planilha.writerow(["hora", "tarefa"])
+        for a in lista_tarefa[1:]:
+            planilha.writerow([lista_tarefa[0], a[0], a[1]])
 
-    for c in lista_tarefa:
-        planilha.writerow([c[0], c[1]])
-
-
+info()
+armazena()
